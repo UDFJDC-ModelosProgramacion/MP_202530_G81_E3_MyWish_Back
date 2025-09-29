9d2474719f0ed812b7f974c315a7472521823b29
Sofia Castro <sscastroq@udistrital.edu.co>
2025-09-28 22:35:05 -0500

    Merge branch 'develop' into feature/UsuarioEntity: integración de cambios y nuevas entidades

---
+++ src/main/java/co/edu/udistrital/mdp/back/entities/CelebracionEntity.java
@@ -0,0 +1,48 @@
+package co.edu.udistrital.mdp.back.entities;
+
+import java.util.Date;
+import java.util.List;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import uk.co.jemos.podam.common.PodamExclude;
+
+@Data
+@Entity
+public class CelebracionEntity extends BaseEntity {
+    
+    private String nombre;
+    private String descripcion;
+    private String lugar;
+    private String color;
+
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+
+    private int cantidadInvitados;
+
+    @PodamExclude
+    @ManyToOne //no propietaria de la relacion
+    private CreadorEntity creador;
+
+    @PodamExclude
+    @ManyToMany //no propietaria de la relacion
+    private List<InvitadoEntity> invitados;
+
+    @PodamExclude
+    @OneToOne(mappedBy = "celebracion")  // propietaria de la relacion
+    private ListaRegalosEntity listaRegalos;
+
+    @PodamExclude
+    @OneToMany(mappedBy = "celebracion") // propietaria de la relacion
+    private List<MensajeInvitacionEntity> mensajesInvitacion;
+
+
+
+
+}
