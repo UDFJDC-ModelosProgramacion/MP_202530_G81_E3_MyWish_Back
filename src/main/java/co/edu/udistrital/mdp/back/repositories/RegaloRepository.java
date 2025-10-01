12cc205dee2c4a005a7d04c0e9b33d7810c0bae8
unknown <saguadab@udistrital.edu.co>
2025-09-30 20:06:04 -0500

    fix: corregir RegaloRepository sin comentarios extra

--- src/main/java/co/edu/udistrital/mdp/back/repositories/RegaloRepository.java
+++ src/main/java/co/edu/udistrital/mdp/back/repositories/RegaloRepository.java
@@ -1,5 +1,10 @@
 package co.edu.udistrital.mdp.back.repositories;
 
-public interface RegaloRepository {
+import co.edu.udistrital.mdp.back.entities.RegaloEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface RegaloRepository extends JpaRepository<RegaloEntity, Long> {
+    
 }
