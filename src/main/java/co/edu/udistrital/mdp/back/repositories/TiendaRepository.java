6e50159eecd5dae41e05194cbc0dc0f73020c5a3
Guada7 <144471109+Guada7@users.noreply.github.com>
2025-09-30 21:17:38 -0500

    Delete src/main/java/co/edu/udistrital/mdp/back/repositories/TiendaRepository.java

--- src/main/java/co/edu/udistrital/mdp/back/repositories/TiendaRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.udistrital.mdp.back.repositories;
-
-import co.edu.udistrital.mdp.back.entities.TiendaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TiendaRepository extends JpaRepository<TiendaEntity, Long> {
-}
