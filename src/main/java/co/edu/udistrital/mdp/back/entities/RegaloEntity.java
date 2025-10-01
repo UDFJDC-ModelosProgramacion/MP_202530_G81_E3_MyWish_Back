99b189418d195e51a189bdf5a18a4ff2f7cdbcbf
unknown <saguadab@udistrital.edu.co>
2025-09-30 20:03:17 -0500

    Fix: completar atributos y anotaciones en RegaloEntity

--- src/main/java/co/edu/udistrital/mdp/back/entities/RegaloEntity.java
+++ src/main/java/co/edu/udistrital/mdp/back/entities/RegaloEntity.java
@@ -1,5 +1,33 @@
 package co.edu.udistrital.mdp.back.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "regalos")
+@Getter
+@Setter
 public class RegaloEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descripcion;
+    private String linkCompra;
+    private Double precioEstimado;
+    private String categoria;
+
+    // Constructor vacío
+    public RegaloEntity() {}
+
+    // Constructor con parámetros
+    public RegaloEntity(String descripcion, String linkCompra, Double precioEstimado, String categoria) {
+        this.descripcion = descripcion;
+        this.linkCompra = linkCompra;
+        this.precioEstimado = precioEstimado;
+        this.categoria = categoria;
+    }
 }
+
