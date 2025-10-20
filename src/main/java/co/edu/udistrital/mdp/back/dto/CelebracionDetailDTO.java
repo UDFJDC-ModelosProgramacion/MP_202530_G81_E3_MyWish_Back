646030b641230af16fcb857abdadb911ed27ed0c
Luis-Lopez <luflopezp@udistrital.edu.co>
2025-10-20 05:36:18 +0000

    feature: cambio nombre detail

--- src/main/java/co/edu/udistrital/mdp/back/dto/CelebracionDetailDTO.java
+++ src/main/java/co/edu/udistrital/mdp/back/dto/CelebracionDetailDTO.java
@@ -4,7 +4,7 @@ import java.util.List;
 import lombok.Data;
 
 @Data
-public class CelebracionDetail extends CelebracionDTO {
+public class CelebracionDetailDTO extends CelebracionDTO {
 
     private List<UsuarioDTO> invitados = new ArrayList<>();
 
