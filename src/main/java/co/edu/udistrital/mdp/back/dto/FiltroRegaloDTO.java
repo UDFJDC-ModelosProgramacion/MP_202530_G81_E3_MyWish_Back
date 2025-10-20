2f5f970d461fc53412716432410a9ea986cac8b9
Sofia Castro <sscastroq@udistrital.edu.co>
2025-10-19 23:22:39 -0500

    Create DTO CatalogoTiendas

---
+++ src/main/java/co/edu/udistrital/mdp/back/dto/FiltroRegaloDTO.java
@@ -0,0 +1,13 @@
+package co.edu.udistrital.mdp.back.dto;
+
+import lombok.Data;
+import lombok.Getter;
+
+import lombok.Setter;
+
+@Data
+
+public class FiltroRegaloDTO {
+    private String criterio;
+    private String valor;
+}
