a8656d1f738b7b41149821b8b473a7aca671c5ee
Wanumen <jruizwanumen@gmail.com>
2025-10-20 02:47:59 -0500

    Agregar los atributos faltantes y eliminar detail

--- src/main/java/co/edu/udistrital/mdp/back/dto/FotoDTO.java
+++ src/main/java/co/edu/udistrital/mdp/back/dto/FotoDTO.java
@@ -4,10 +4,25 @@ import lombok.Data;
 
 @Data
 public class FotoDTO {
+
     private Long id;
+
     private String url;
+
     private String descripcion;
+
     private String tipoArchivo;
+
     private Long tamanioBytes;
+
     private Boolean esPrincipal;
+
+    private UsuarioDTO usuario;
+
+    private ListaRegalosDTO listaRegalos;
+
+    private TiendaDTO tienda;
+
+    private ComentarioDTO comentario;
+
 }
