fb5a11c3471239d78e5f81465e4bab6d2f531881
Luis-Lopez <luflopezp@udistrital.edu.co>
2025-10-20 08:10:10 +0000

    feature: creacion archivos faltantes

---
+++ src/main/java/co/edu/udistrital/mdp/back/dto/TiendaDetailDTO.java
@@ -0,0 +1,16 @@
+package co.edu.udistrital.mdp.back.dto;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class TiendaDetailDTO extends TiendaDTO {
+
+    private List<UbicacionDTO> ubicaciones = new ArrayList<>();
+    private List<ComentarioDTO> comentarios = new ArrayList<>();
+    private List<RegaloDTO> regalos = new ArrayList<>();
+    private List<FotoDTO> fotos = new ArrayList<>();
+}
